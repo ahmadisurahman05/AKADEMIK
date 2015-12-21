e9c93f96b7d4f75e09cbd53c47d4a86d8572b401
ahmadisurahman05 <madozahmad123@gmail.com>
2015-12-21 08:28:36 -0800

    masukan data baru

---
+++ about.php
@@ -0,0 +1,11 @@
+<div class="popup-wrapper" id="popup">
+<div class="popup-container" style="font-style:normal; font-family:'Arial Black', Gadget, sans-serif; text-transform:none;">
+yPOS merupakan sebuah sistem yang dibangun dengan tujuan untuk memudahkan setiap kegiatan bisnis anda.
+<br/>
+Untuk kritik dan masukan, silahkan hubungi saya di yudaadp@gmail.com.<br/>
+Untuk mengetahui update terbaru, silahkan daftarkan email (kirim email) ke saya dengan subject "yPOS UPDATE". Jika ada updatean terbaru, anda akan diberitahu via email.
+<br/><br/>
+&copy; 2015 yPOS v1.0.1 By Yuda
+<a class="popup-close" href="#popup">X</a>
+</div>
+</div>
